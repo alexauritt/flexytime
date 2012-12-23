d9cc45919b2e4a45e9ebab2f14f4acd1c75f6caf
Alex Auritt <alexauritt@gmail.com>
2012-12-23 13:16:05 -0500

    starter rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlexBourbon::Application.config.secret_token = '57142e87799a6d19dc7f47740b27be239d66fdc5463328c4a13b88cad571946fa0b0f1fe4b13a77cc5be252808b66ec2d52e83af579082d83aea9d2010faf357'
